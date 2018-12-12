2aae5d47e12243d034936796cee3c30e30c9187e
GyDi <segydi@foxmail.com>
2018-12-12 23:10:46 +0800

    .

--- src/main.js
+++ src/main.js
@@ -2,17 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router.js";
 import ElementUI from "element-ui";
-import 'element-ui/lib/theme-chalk/index.css'
-import global_ from './components/Global.js'
+import 'element-ui/lib/theme-chalk/index.css';
+import global_ from './components/Global.js';
 
 
-Vue.use(ElementUI)
+Vue.use(ElementUI);
 Vue.config.productionTip = false;
 
-Vue.prototype.GLOBAL = global_
+Vue.prototype.GLOBAL = global_;
 new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
-
-
